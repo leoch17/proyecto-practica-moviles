cee3b4c755789bea794c9d043281ceb72d6832c6
Leonardo Chourio <leoch1000@gmail.com>
2021-06-03 11:51:02 -0400

    init

--- src/rutas/peliculas.js
+++ src/rutas/peliculas.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const _ = require("underscore");
 
 const peliculas = require("../ejemplo.json");
 
@@ -8,8 +9,24 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("recibido");
+  const { title, director, year, prime, nominations, raiting } = req.body;
+  if (title && director && year && prime && nominations && raiting) {
+    const id = peliculas.length + 1;
+    const peliculasnew = { ...req.body, id };
+    peliculas.push(peliculasnew);
+    res.json(peliculas);
+  } else {
+    res.status(500).json({ error: "Ha ocurrido un error" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  _.each(peliculas, (pelicula, i) => {
+    if (pelicula.id == id) {
+      peliculas.splice(i, 1);
+    }
+  });
+  res.send(peliculas);
 });
 
 module.exports = router;
